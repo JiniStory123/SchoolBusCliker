1b75eef9783d5d790ce9df483bfacbbc2f8ab35f
JiniStory123 <jinistory123@gmail.com>
2023-04-03 12:59:41 +0900

    임시로

---
+++ SchoolBusClickerDriver/app/src/main/java/com/cafe24/as8794/schoolbusclickerdriver/MainActivity.java
@@ -0,0 +1,18 @@
+package com.cafe24.as8794.schoolbusclickerdriver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+    }
+}
